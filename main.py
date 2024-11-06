13a6cbc7c0ddc9ef001d2702dd280626f8dca578
TheTesterss <morganjaouen65636@gmail.com>
2024-11-06 11:33:41 +0100

    Update main.py

--- main.py
+++ main.py
@@ -4,43 +4,25 @@ from File import File
 from Pile import Pile
 import random
 
-""" Demo of each methods that can be print avoid 3 lines to be copied x times """
-def demoFile():
-    global list
+""" Show the list """
+def show(list):
     # * print(list.isEmpty())
     # * print(list.Length())
     list.Show()
 
-""" Demo of each methods that can be print avoid 3 lines to be copied x times """
-def demoPile():
-    global list_1
-    # * print(list_1.isEmpty())
-    # * print(list_1.Length())
-    list_1.Show()
-
-# ! Test File
-list = File()
-demoFile()
-for i in range(15):
-    list.Insert(random.randint(0, 100))
-demoFile()
-for i in range(5):
+""" Build a list depending from the classe """
+def build(Classe):
+    list = Classe()
+    show(list)
+    for i in range(15):
+        list.Insert(random.randint(0, 100))
+    show(list)
+    for i in range(5):
     list.Remove()
-demoFile()
+    show(list)
 
-print(list.At(random.randint(0, 10)))
+    print(list.At(random.randint(0, 10)))
 
+build(File)
 print()
-
-# ! Test Pile
-
-list_1 = Pile()
-demoPile()
-for i in range(15):
-    list_1.Insert(random.randint(0, 100))
-demoPile()
-for i in range(5):
-    list_1.Remove()
-demoPile()
-
-print(list_1.At(random.randint(0, 10)))
+build(Pile)
